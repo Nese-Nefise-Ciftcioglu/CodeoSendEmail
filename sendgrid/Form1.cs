418c37c0b2a4933eb9b3d16483ef5bb7495c6d6d
Nese-Nefise-Ciftcioglu <94238138+Nese-Nefise-Ciftcioglu@users.noreply.github.com>
2023-07-25 11:45:46 +0300

    Attaching Image

--- sendgrid/Form1.cs
+++ sendgrid/Form1.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+
 namespace sendgrid
 {
     public partial class Form1 : Form
@@ -16,11 +18,11 @@ namespace sendgrid
         public Form1()
         {
             InitializeComponent();
-        }     
+        }
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            var apiKey = "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";
+            var apiKey = "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";
             //Api Key should be saved in environment variables for security issues: 
             //var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
             var client = new SendGridClient(apiKey);
@@ -34,12 +36,18 @@ namespace sendgrid
 
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
 
+            //Attach image
+            var imagepPath = "D:/Desktop/deneme.png";
+            var bytes = File.ReadAllBytes(imagepPath);
+            var base64Image = Convert.ToBase64String(bytes);
+            msg.AddAttachment("image.jpg", base64Image);
+
             try
             {
                 var response = await client.SendEmailAsync(msg);
                 if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
                 {
-                    MessageBox.Show("Email sent successfully!");
+                    //MessageBox.Show("Email sent successfully!");
                     Application.Exit();  // Exit the application once email is sent
                 }
                 else
@@ -48,7 +56,7 @@ namespace sendgrid
                 }
             }
             catch (Exception ex)
-            {               
+            {
                 MessageBox.Show(ex.Message);
             }
         }
